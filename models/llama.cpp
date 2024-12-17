foldl/chatllm.cpp

commit b53c899381b999503b3a7f74be39b17f076e0f24

more models works with vulkan: granite, minicpm, llama3.1, etc.

models/llama.cpp
@@ -123,7 +123,8 @@ If a question does not make any sense, or is not factually coherent, explain why
                 loader.read_tensor("lm_head.weight", dynamic_cast<Linear *>(transformer->lm_head)->weight);
 
             CHATLLM_CHECK(w_ctx_.get_used_mem() == w_ctx_.get_mem_size())
-                << "corrupted model weights";
+                << "corrupted model weights: " << w_ctx_.get_used_mem() / ggml_tensor_overhead() << " vs "
+                << w_ctx_.get_mem_size() / ggml_tensor_overhead();
         }
 
     public:
@@ -461,7 +462,7 @@ namespace v3_1
         const float low_freq_wavelen = old_context_len / low_freq_factor;
         const float high_freq_wavelen = old_context_len / high_freq_factor;
 
-        for (int i = 0; i < rope_dim; i++)
+        for (int i = 0; i < (int)inv_freq.size(); i++)
         {
             const float freq = inv_freq[i];
             const float wavelen = 2 * std::numbers::pi_v<float> / freq;
@@ -481,7 +482,7 @@ namespace v3_1
         }
     }
 
-    class ConditionalGeneration : public v2::GenericConditionalGeneration<LlamaBlock>
+    class ConditionalGeneration : public v2::GenericConditionalGeneration<Llama31Block>
     {
     public:
         ConditionalGeneration() = default;
@@ -490,31 +491,27 @@ namespace v3_1
         {}
 
         ConditionalGeneration(const Config &config, const RuntimeConfig &runtime_config, ModelType type,
-                            int num_key_value_heads, int max_length, int tensors_per_layer = 12, bool tie_lm_head = false, int additional_tensor = 1)
-            : v2::GenericConditionalGeneration<LlamaBlock>(config, runtime_config, type, num_key_value_heads, max_length, tensors_per_layer, tie_lm_head, additional_tensor)
+                            int num_key_value_heads, int max_length, int tensors_per_layer = 13, bool tie_lm_head = false, int additional_tensor = 0)
+            : v2::GenericConditionalGeneration<Llama31Block>(config, runtime_config, type, num_key_value_heads, max_length, tensors_per_layer, tie_lm_head, additional_tensor)
         {
-            freq_factors = ggml::new_tensor_1d(&w_ctx_, GGML_TYPE_F32, config.hidden_size / config.num_attention_heads);
-
             init_llama3_freq_factors(freq_factors_value, config.hidden_size / config.num_attention_heads,
                                       config.rope_theta,
                                       config.rope_scaling_factor,
                                       config.rope_scaling_low_freq_factor,
                                       config.rope_scaling_high_freq_factor,
                                       config.rope_scaling_original_max_position_embeddings);
-            Backend::write_tensor_data(freq_factors, freq_factors_value.data());
 
             auto transformer = Base::get_typed_transformer<ModelClass>();
             for (int i = 0; i < config.num_hidden_layers; i++)
             {
                 auto &attention = transformer->layers[i].attention;
-                attention.freq_factors = freq_factors;
                 attention.freq_base    = config.rope_theta;
+                Backend::write_tensor_data(attention.freq_factors, freq_factors_value.data());
             }
         }
 
         ChunkInterceptor *get_interceptor(void) override { return &interceptor; }
     protected:
-        ggml::tensor *freq_factors;
         std::vector<float> freq_factors_value;
     };
 }

src/backend.cpp
@@ -27,239 +27,8 @@ extern void ggml_vk_print_devices_info(void);
 
 #include "ggml-cpu.h"
 
-#if (0)
-#include "ggml-impl.h"
-#include "ggml-backend-impl.h"
-#endif
-
 namespace chatllm
 {
-#if (0)
-    #define PRId64 "lld"
-
-    static struct ggml_tensor * ggml_graph_get_parent(const struct ggml_cgraph * cgraph, const struct ggml_tensor * node) {
-        for (int i = 0; i < cgraph->n_nodes; i++) {
-            struct ggml_tensor * parent = cgraph->nodes[i];
-            struct ggml_tensor * grad = ggml_graph_get_grad(cgraph, parent);
-
-            if (grad == node) {
-                return parent;
-            }
-        }
-
-        return NULL;
-    }
-
-    static void ggml_graph_dump_dot_node_edge(FILE * fp, const struct ggml_cgraph * gb, struct ggml_tensor * node, struct ggml_tensor * parent, const char * label)  {
-        struct ggml_tensor * gparent = ggml_graph_get_parent(gb, node);
-        struct ggml_tensor * gparent0 = ggml_graph_get_parent(gb, parent);
-        fprintf(fp, "  \"%p\":%s -> \"%p\":%s [ arrowhead = %s; style = %s; label = \"%s\"; ]\n",
-                gparent0 ? (void *) gparent0 : (void *) parent,
-                gparent0 ? "g" : "x",
-                gparent ? (void *) gparent : (void *) node,
-                gparent ? "g" : "x",
-                gparent ? "empty" : "vee",
-                gparent ? "dashed" : "solid",
-                label);
-    }
-
-    static void ggml_graph_dump_dot_leaf_edge(FILE * fp, struct ggml_tensor * node, struct ggml_tensor * parent, const char * label)  {
-        fprintf(fp, "  \"%p\":%s -> \"%p\":%s [ label = \"%s\"; ]\n",
-                (void *) parent, "x",
-                (void *) node, "x",
-                label);
-    }
-
-    static uint32_t simple_hash(const unsigned char *str)
-    {
-        uint32_t hash = 5381;
-        unsigned char c;
-
-        while (c = *str++) hash = hash * 33 + c;
-
-        return hash;
-    }
-
-    static const char *ggml_color_of_backend(ggml_backend_sched_t sched, struct ggml_tensor * node) {
-        #define COLOR_NUM   12
-
-        static char color[32];
-        uint32_t color1 = 0;
-        uint32_t color2 = 0;
-
-        ggml_backend_t backend = ggml_backend_sched_get_tensor_backend(sched, node);
-        if (backend) {
-            const char *name = ggml_backend_name(backend);
-            color1 = simple_hash((const unsigned char *)name) % COLOR_NUM;
-            color2 = color1;
-        }
-        if (node->buffer) {
-            ggml_backend_buffer_type_t buft = node->buffer->buft;
-            if (backend && !ggml_backend_supports_buft(backend, buft))
-            {
-                color2 = simple_hash((const unsigned char *)ggml_backend_buft_name(buft)) % COLOR_NUM;
-                if (color2 == color1) color2 = (color1 + 1) % COLOR_NUM;
-            }
-        }
-        snprintf(color, sizeof(color), "%d;0.5:%d", color1 + 1, color2 + 1);
-        return color;
-    }
-
-    static void ggml_graph_dump_leaf(ggml_backend_sched_t sched, FILE * fp, std::set<void *> &visited_nodes, const struct ggml_cgraph * gb, const struct ggml_cgraph * gf, struct ggml_tensor * node, struct ggml_tensor *parent, const char *label, const int i) {
-        char color[16];
-
-        if (visited_nodes.find(node) != visited_nodes.end()) {
-            goto draw_edge;
-        }
-
-        visited_nodes.insert(node);
-
-        snprintf(color, sizeof(color), "pink");
-
-        fprintf(fp, "  \"%p\" [ "
-                    "style = filled; fillcolor = \"%s\"; shape = record; "
-                    "label=\"<x>",
-                (void *) node, ggml_color_of_backend(sched, node));
-
-        if (strlen(node->name) > 0) {
-            fprintf(fp, "%s (%s)|", node->name, ggml_type_name(node->type));
-        } else {
-            fprintf(fp, "(%s)|", ggml_type_name(node->type));
-        }
-
-        fprintf(fp, "CONST %d [%" PRId64 ", %" PRId64 "]", i, node->ne[0], node->ne[1]);
-        fprintf(fp, "\"; ]\n");
-
-    draw_edge:
-        if (parent) {
-            ggml_graph_dump_dot_leaf_edge(fp, parent, node, label);
-        }
-        if (node->view_src) {
-            ggml_graph_dump_dot_leaf_edge(fp, node, node->view_src, label);
-        }
-    }
-
-    static bool ggml_graph_is_leaf(struct ggml_tensor * node) {
-        return node->op == GGML_OP_NONE && !(node->flags & GGML_TENSOR_FLAG_PARAM);
-    }
-
-    // check if node is part of the graph
-    static bool ggml_graph_find(const struct ggml_cgraph * cgraph, const struct ggml_tensor * node) {
-        if (cgraph == NULL) {
-            return true;
-        }
-
-        for (int i = 0; i < cgraph->n_nodes; i++) {
-            if (cgraph->nodes[i] == node) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    static void ggml_graph_dump_dot_node(ggml_backend_sched_t sched, FILE * fp, std::set<void *> &visited_nodes, const struct ggml_cgraph * gb, const struct ggml_cgraph * gf, struct ggml_tensor * node, struct ggml_tensor *child, const char *label, const int i) {
-        char color[16];
-        struct ggml_tensor * grad = nullptr;
-
-        if (ggml_graph_is_leaf(node))
-        {
-            ggml_graph_dump_leaf(sched, fp, visited_nodes, gb, gb, node, child, label, i);
-            return;
-        }
-
-        if (visited_nodes.find(node) != visited_nodes.end()) {
-            goto draw_edge;
-        }
-
-        visited_nodes.insert(node);
-
-        grad = ggml_graph_get_grad(gb, node);
-
-        if (node->flags & GGML_TENSOR_FLAG_PARAM) {
-            snprintf(color, sizeof(color), "yellow");
-        } else if (grad) {
-            if (ggml_graph_find(gf, node)) {
-                snprintf(color, sizeof(color), "green");
-            } else {
-                snprintf(color, sizeof(color), "lightblue");
-            }
-        } else {
-            snprintf(color, sizeof(color), "white");
-        }
-
-        fprintf(fp, "  \"%p\" [ "
-                    "style = filled; fillcolor = \"%s\"; shape = record;"
-                    "label=\"",
-                (void *) node, ggml_color_of_backend(sched, node));
-
-        if (strlen(node->name) > 0) {
-            fprintf(fp, "%s (%s)|", node->name, ggml_type_name(node->type));
-        } else {
-            fprintf(fp, "(%s)|", ggml_type_name(node->type));
-        }
-
-        if (ggml_is_matrix(node)) {
-            fprintf(fp, "%d [%" PRId64 ", %" PRId64 "] | <x>%s", i, node->ne[0], node->ne[1], ggml_op_symbol(node->op));
-        } else {
-            fprintf(fp, "%d [%" PRId64 ", %" PRId64 ", %" PRId64 "] | <x>%s", i, node->ne[0], node->ne[1], node->ne[2], ggml_op_symbol(node->op));
-        }
-
-        if (grad) {
-            fprintf(fp, " | <g>%s\"; ]\n", ggml_op_symbol(grad->op));
-        } else {
-            fprintf(fp, "\"; ]\n");
-        }
-
-        for (int j = 0; j < GGML_MAX_SRC; j++) {
-            if (node->src[j]) {
-                char label[16];
-                snprintf(label, sizeof(label), "src %d", j);
-
-                ggml_graph_dump_dot_node(sched, fp, visited_nodes, gb, gf, node->src[j], node, label, -1);
-            }
-        }
-
-    draw_edge:
-        if (child) {
-            ggml_graph_dump_dot_node_edge(fp, gb, child, node, label);
-        }
-    }
-
-    void ggml::graph_dump_dot(ggml_backend_sched_t sched, const struct ggml_cgraph * gb, const struct ggml_cgraph * gf, const char * filename) {
-
-        FILE * fp = ggml_fopen(filename, "w");
-        GGML_ASSERT(fp);
-
-        std::set<void *> visited_nodes;
-
-        fprintf(fp, "digraph G {\n");
-        fprintf(fp, "node [colorscheme=set312]\n");
-        fprintf(fp, "  newrank = true;\n");
-        fprintf(fp, "  rankdir = TB;\n");
-
-        for (int i = 0; i < gb->n_nodes; i++) {
-            struct ggml_tensor * node = gb->nodes[i];
-
-            if (ggml_graph_get_parent(gb, node) != NULL) {
-                continue;
-            }
-
-            ggml_graph_dump_dot_node(sched, fp, visited_nodes, gb, gf, node, NULL, NULL, i);
-        }
-
-        //extern void ggml_backend_dump_splits(ggml_backend_sched_t sched, const struct ggml_cgraph * graph, FILE * fp);
-
-        ggml_backend_sched_splits_fdump_dot(fp, sched, gb);
-
-        fprintf(fp, "}\n");
-
-        fclose(fp);
-
-        GGML_LOG_INFO("%s: dot -Tpng %s -o %s.png && open %s.png\n", __func__, filename, filename, filename);
-    }
-#endif
-
     void *BackendBuffer::get_base(void)
     {
         return ggml_backend_buffer_get_base(buf);
@@ -860,7 +629,11 @@ namespace chatllm
     void ComputeContext::cb_op_tensor(ggml::tensor *tensor)
     {
         if (get_backend() && ggml_backend_supports_op(get_backend()->backend, tensor))
+        {
+            //struct ggml_backend_buffer *buffer = tensor->buffer;
+            //if (buffer && ggml_backend_supports_buft(get_backend()->backend, ggml_backend_buffer_get_type(buffer)))
             ggml_backend_sched_set_tensor_backend(get_sched(), tensor, get_backend()->backend);
+        }
     }
 
     ggml_backend_sched_t ComputeContext::get_sched(void)

src/layers.h
@@ -1766,6 +1766,32 @@ namespace chatllm
         {}
     };
 
+    class Llama31SelfAttention : public RoPESelfAttention<BaseAttention>
+    {
+    public:
+        Llama31SelfAttention(InitContext *ctx, int hidden_size, int num_attention_heads, int max_length)
+            : RoPESelfAttention(ctx, hidden_size, num_attention_heads, max_length, false, false)
+        {
+            freq_factors = ggml::new_tensor_1d(ctx, GGML_TYPE_F32, hidden_size / num_attention_heads / 2);
+            ctx->get_allocator()->alloc(freq_factors);
+        }
+
+        Llama31SelfAttention(InitContext *ctx, int hidden_size, int num_attention_heads, int num_kv_heads, int max_length)
+            : RoPESelfAttention(ctx, hidden_size, num_attention_heads, num_kv_heads, max_length, false, false)
+        {
+            freq_factors = ggml::new_tensor_1d(ctx, GGML_TYPE_F32, hidden_size / num_attention_heads / 2);
+            ctx->get_allocator()->alloc(freq_factors);
+        }
+    };
+
+    class Llama31Block : public LMBlock1<RMSNorm, Llama31SelfAttention, RMSNorm, SiLUMLP>
+    {
+    public:
+        Llama31Block(InitContext *ctx, int hidden_size, int num_attention_heads, int intermediate_size, int num_kv_heads, int max_length)
+            : LMBlock1(ctx, hidden_size, num_attention_heads, intermediate_size, num_kv_heads, max_length)
+        {}
+    };
+
     class RobertaPooler : public Block
     {
     public:

commit d5d294c579a79d9a4376a314499128b95b59b4c4

fix regression caused by b53c899381b999503b3a7f74be39b17f076e0f24

models/llama.cpp
@@ -531,7 +531,7 @@ namespace v3_2
         ConditionalGeneration() = default;
         ConditionalGeneration(const Config &config, const RuntimeConfig &runtime_config, ModelType type = ModelType::MODEL_TYPE_LLAMA3_1)
             : v3_1::ConditionalGeneration(config, runtime_config, type, config.num_key_value_heads, config.max_length,
-                                          12, config.tie_word_embeddings != 0, 1)
+                                          13, config.tie_word_embeddings != 0, 0)
         {}
     };
 }